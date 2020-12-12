f97cd0b059f5e16b792497dd25c292fa793ac797
breno-almeida <breno9985260490@gmail.com>
2020-12-12 14:45:50 -0300

    Restructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,6 @@ import { Flex, Heading, Image } from '@chakra-ui/react'
 
 import Head from 'next/head'
 
-import GiveClassesImg from '~/assets/images/icons/give-classes.svg'
-import PurpleHeartImg from '~/assets/images/icons/purple-heart.svg'
-import StudyImg from '~/assets/images/icons/study.svg'
-import LandingImg from '~/assets/images/landing.svg'
-import LogoImg from '~/assets/images/logo.svg'
-
 import Container from '~/Components/Container'
 
 export default function Home() {
